47df1015df507dd3fb9abe3e1e090a7f4571c3fd
zxy <zhangxyg@qq.com>
2025-02-12 17:27:34 +0800

    修改import

--- sample/src/main/java/com/obelieve/frame/sample/MainActivity.java
+++ sample/src/main/java/com/obelieve/frame/sample/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import com.obelieve.frame.R;
 import com.obelieve.frame.base.ApiBaseActivity2;
-import com.obelieve.frame.databinding.ActivityMainBinding;
+import com.obelieve.frame.sample.databinding.ActivityMainBinding;
 
 public class MainActivity extends ApiBaseActivity2<ActivityMainBinding> {
 
